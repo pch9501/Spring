cf043280b47d16d6c321c5ccae34c5b7ecc30941
parkchanhwi <pcj9501@naver.com>
2020-06-10 10:36:05 +0900

    BasicProject-4

--- SpringBasicProject3/src/main/java/com/sist/spring2/EmpMapper.java
+++ SpringBasicProject3/src/main/java/com/sist/spring2/EmpMapper.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 
+//MyBatis인데 XML 없이 코딩
 public interface EmpMapper {
 	@Select("SELECT * FROM emp")
-	public List<EmpVO> empAllData();
+	public List<EmpVO> empAllData();	
+	// 	   ==========            ==
+	//     ResultType            parameterType
+	
+	// 위의 코드는 아래의 XML 코드와 같은 역할 
+	// <select id="empAllData" resyltType="EmpVO">SELECT * FROM emp</select>
 }
