437201cf204d98969f769b86c49d3f9e1a82eb66
parkchanhwi <pcj9501@naver.com>
2020-06-10 10:39:08 +0900

    BasicProject-5

---
+++ SpringBasicProject4/src/main/java/com/sist/spring/MainClass.java
@@ -0,0 +1,26 @@
+package com.sist.spring;
+
+import java.util.*;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sist.dao.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. XML 파일 읽기 
+		ApplicationContext app=
+				new ClassPathXmlApplicationContext("app.xml");
+		// 2. getBean으로 클래스 객체 찾아서 얻어온다
+		EmpDAO dao=(EmpDAO)app.getBean("dao");
+		List<EmpVO> list=dao.empAllData();
+		for(EmpVO vo:list)
+		{
+			System.out.println(vo.getEname()+" "+vo.getJob()+" ");
+		}
+	}
+
+}
